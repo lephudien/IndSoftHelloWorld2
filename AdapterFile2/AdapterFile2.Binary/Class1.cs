1cba251065ddd0814ee985457d8bde8a0f5c3757
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-05 13:21:54 +0200

    Zalozeni AdapterFile2

--- AdapterFile2/AdapterFile2.Binary/Class1.cs
+++ AdapterFile2/AdapterFile2.Binary/Class1.cs
@@ -5,18 +5,21 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdapterDB2.MSSQL
+namespace AdapterFile2.Binary
 {
-  public class ReadFromDB
+  public class BinaryRW
   {
     public static string GetMyVersion()
     {
       System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
       System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
 
-      Version version = Assembly.GetAssembly(typeof(System.Data.SqlClient.SqlAuthenticationMethod)).GetName().Version;
+      Version version = Assembly.GetAssembly(typeof(Google.Apis.Logging.ConsoleLogger)).GetName().Version;
 
-      return $"AdapterDB.MSSQL Ver={fvi.FileVersion}{Environment.NewLine}Using EXTERNAL MSSQLClientVer{version.ToString()}";
+      //return $"AdapterDB.OracleVer={fvi.FileVersion}{Environment.NewLine}Using EXTERNAL OracleVer{version.ToString()}";
+
+      
+      return $"AdapterFile.Binary Ver={fvi.FileVersion}{Environment.NewLine}Using EXTERNAL Google.Apis Ver{version.ToString()}";
     }
   }
 }
--- AdapterFile2/AdapterFile2.Text/Class1.cs
+++ AdapterFile2/AdapterFile2.Text/Class1.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdapterDB2.MSSQL
+namespace AdapterFile2.Text
 {
-  public class ReadFromDB
+  public class TextRW
   {
     public static string GetMyVersion()
     {
       System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
       System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
 
-      Version version = Assembly.GetAssembly(typeof(System.Data.SqlClient.SqlAuthenticationMethod)).GetName().Version;
-
-      return $"AdapterDB.MSSQL Ver={fvi.FileVersion}{Environment.NewLine}Using EXTERNAL MSSQLClientVer{version.ToString()}";
+      return "AdapterFile.Text Ver=" + fvi.FileVersion;
     }
   }
 }
