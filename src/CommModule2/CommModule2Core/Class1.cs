6338d76d755ee49e3f6a07fbda6f69bfe0330158
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-05 14:16:03 +0200

    CommModule2

---
+++ src/CommModule2/CommModule2Core/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommModule2Core
+{
+  public class CommCore
+  {
+    public static string GetMyVersion()
+    {
+      //int a = Common.Constants.CONSTANTY.C_NEJAKA_VERZE;
+      //var test = new Oracle.ManagedDataAccess.Client.OracleConnection();
+      //Debug.Assert(test == null);
+      //var test2 = new NUnit.Framework.Constraints.AndOperator();
+
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+      System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+
+      Version version = Assembly.GetAssembly(typeof(SimpleTCP.Server.TcpListenerEx)).GetName().Version;
+
+      return $"AdapterDB.Oracle Ver={fvi.FileVersion}{Environment.NewLine}Using EXTERNAL SimpleTCP Ver{version.ToString()}";
+    }
+  }
+}
