da349aa9b6743b9a37eda800aa0ba8a9316f017c
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-05 15:26:58 +0200

    AppServer

---
+++ src/AppServer2/AppServer2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppServer2
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+      System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+
+      System.Windows.Forms.MessageBox.Show($"AppServer2 Ver={fvi.FileVersion}{Environment.NewLine}Using {ServerClientComm.ServerClientComm.GetMyVersion()}" +
+                                          $"{Environment.NewLine}Using {LoggerChildDB.LogDB.GetMyVersion()}" +
+                                          $"{Environment.NewLine}Using {LoggerChildBinary.LogBinary.GetMyVersion()}" +
+                                          $"{Environment.NewLine}Using {CommModule2Core.CommCore.GetMyVersion()}");
+    }
+  }
+}
---
+++ src/AppServer2/ServerClientComm/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerClientComm
+{
+  public class ServerClientComm
+  {
+    public static string GetMyVersion()
+    {
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+      System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+
+      return "ServerClientComm Ver=" + fvi.FileVersion;
+    }
+  }
+}
