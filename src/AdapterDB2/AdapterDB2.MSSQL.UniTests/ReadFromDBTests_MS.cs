1fbc93635597715aaff32b6ac9c62454c9c3f758
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-21 09:50:27 +0200

    Unit Test MS

--- src/AdapterDB2/AdapterDB2.MSSQL.UniTests/ReadFromDBTests_MS.cs
+++ src/AdapterDB2/AdapterDB2.MSSQL.UniTests/ReadFromDBTests_MS.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,39 +7,39 @@ using System.Threading.Tasks;
 
 namespace AdapterDB2.MSSQL.UniTests
 {
-  [TestFixture]
-  public class ReadFromDBTests_NUnit
+  [TestClass]
+  public class ReadFromDBTests_MS
   {
-    [Test]
+    [TestMethod]
     public void IsValidConnectString_ReturnTrue()
     {
       string sMyConnectString = "MSSQL;Server=lanceddb04;Database=evcs_cpovyvoj;User Id=Lancelot_EVCS;Password=Deneb2;";
       ReadFromDB test = new ReadFromDB();
       bool oResult = test.Connect(sMyConnectString);
 
-      Assert.True(oResult);
+      Assert.IsTrue(oResult);
     }
 
-    [Test]
+    [TestMethod]
     public void IsNotValidConnectString_ReturnFalse()
     {
       string sMyConnectString = "Oracle;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.18.6.88)(PORT=1522))(CONNECT_DATA=(SID=ORCL)));User ID=GAS_OTE_DATA;Password=x";
       ReadFromDB test = new ReadFromDB();
       bool oResult = test.Connect(sMyConnectString);
 
-      Assert.False(oResult);
+      Assert.IsFalse(oResult);
     }
 
-    [Test]
-    [Ignore("there is a problem with this test")]
+    [TestMethod]
+    [Ignore()] //"there is a problem with this test"
     public void IsValidConnectString_ReturnTrue_SchvalneChyba()
     {
-      string sMyConnectString = "MSxxxSQL;Server=lanceddb04;Database=evcs_cpovyvoj;User Id=Lancelot_EVCS;Password=Deneb2;";
-      //string sMyConnectString = "MSSQL;Server=lanceddb04;Database=evcs_cpovyvoj;User Id=Lancelot_EVCS;Password=Deneb2;";
+      //string sMyConnectString = "MSxxxSQL;Server=lanceddb04;Database=evcs_cpovyvoj;User Id=Lancelot_EVCS;Password=Deneb2;";
+      string sMyConnectString = "MSSQL;Server=lanceddb04;Database=evcs_cpovyvoj;User Id=Lancelot_EVCS;Password=Deneb2;";
       ReadFromDB test = new ReadFromDB();
       bool oResult = test.Connect(sMyConnectString);
 
-      Assert.True(oResult);
+      Assert.IsTrue(oResult);
     }
   }
 }
